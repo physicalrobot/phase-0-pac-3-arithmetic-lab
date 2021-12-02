be55f333c864bbf4a568137269c926ba1bf0f233
Vikalp Malhotra <kimboho@gmail.com>
2021-12-01 21:27:48 -0500

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(x, y) {
+    let a = x + y;
+    return a;
+}
+
+function subtract(x, y) {
+    let a = x - y;
+    return a;
+}
+
+function multiply(x, y) {
+    let a = x * y;
+    return a;
+}
+
+function divide(x, y) {
+    let a = x / y;
+    return a;
+}
+
+function increment(n) {
+    ++n;
+    return n;
+}
+
+function decrement(n) {
+
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+
+    return parseInt(n, 10);
+
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
